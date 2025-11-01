5d9b19386074723d7dd92eca687464e3e54285c1
Barbieso <tangkwatangkwa1032@gmail.com>
2025-11-01 11:03:18 +0700

    daily_vocab_api

--- api/main.py
+++ api/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from app.schemas import WordResponse
+from fastapi import HTTPException
+from app.routers import words
 
 # Initialize FastAPI app
 app = FastAPI(
@@ -7,25 +10,35 @@ app = FastAPI(
     description="API for vocabulary practice and learning"
 )
 
-@app.get("/api/word")
-def get_random_word():
-    """Get a random word"""
-    # TODO Write logic here....
-    return {
-        "word": "example",
-        "definition": "a representative form or pattern",
-        "difficulty_level": "Beginner"
-    }
 
-@app.get("/")
-def read_root():
-    return {
-        "message": "Vocabulary Practice API",
-        "version": "1.0.0",
-        "endpoints": {
-            "random_word": "/api/word",
-            "validate": "/api/validate-sentence",
-            "summary": "/api/summary",
-            "history": "/api/history"
-        }
-    }
+app.include_router(words.router, prefix="/api", tags=["words"])
+
+#@app.get("/api/word", response_model=WordResponse)
+#def get_random_word():
+#    """Get a random word"""
+#    # TODO Write logic here....
+#    return WordResponse(
+#        id=2,
+#        word='book',
+#        definition='Reading material',
+#        difficulty_level='Beginner'
+#)
+# words = []
+# if len(words) == 0:
+#    raise HTTPException(
+#        status_code=404,
+#        detail="No words availabel in database"
+#    )
+
+#@app.get("/")
+#def read_root():
+#    return {
+#        "message": "Vocabulary Practice API",
+#        "version": "1.0.0",
+#        "endpoints": {
+#            "random_word": "/api/word",
+#            "validate": "/api/validate-sentence",
+#            "summary": "/api/summary",
+#            "history": "/api/history"
+#        }
+#    }
